d5048986acb3eca654d60ef31beb50d123e34d74
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2024-02-12 20:09:57 -0500

    portfolio commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import Projects from './Components/Projects';
+import BlogPost from './Components/BlogPost';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+     <Navbar />
+     <Routes>
+     {/* <Route path="/" element={/Home} /> */}
+     <Route path="/" element={<Home/>} />
+     <Route path="/Projects" element={<Projects/>} />
+     <Route path="/BlogPost" element={<BlogPost/>} />
+     <Route path="/" element={<Home/>} />
+     {/* <Route path='/Projects' element={/Projects>} /> */}
+
+
+
+     </Routes>
+     </Router>
     </div>
   );
 }
